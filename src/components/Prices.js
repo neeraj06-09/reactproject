aaebe166741d2336bf7d9c3ebac651127ad50019
Neeraj Pandey <61829291+neeraj06-09@users.noreply.github.com>
2022-01-18 01:03:06 +0530

    images update

--- src/components/Prices.js
+++ src/components/Prices.js
@@ -8,7 +8,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/starter.jpg" alt="starter" />
+                <img src="https://uspizza.in/images/home-box1.jpg" alt="starter" />
               </div>
               <h1 className="price__heading">Starter</h1>
               <p className="price__text">
@@ -21,7 +21,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/Pizza_menu.jpg" alt="pizza" />
+                <img src="https://uspizza.in/images/home-box2.jpg" alt="pizza" />
               </div>
               <h1 className="price__heading">Pizza</h1>
               <p className="price__text">
@@ -34,7 +34,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/pasta.jpg" alt="pasta" />
+                <img src="https://uspizza.in/images/home-box3.jpg" alt="pasta" />
               </div>
               <h1 className="price__heading">Pasta</h1>
               <p className="price__text">
@@ -47,7 +47,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/drink.jpg" alt="drink" />
+                <img src="https://uspizza.in/images/home-box4.jpg" alt="drink" />
               </div>
               <h1 className="price__heading">Cafe Casa Blanca</h1>
               <p className="price__text">
@@ -60,7 +60,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/burger.jpg" alt="chicken" />
+                <img src="https://uspizza.in/images/home-box5.jpg" alt="chicken" />
               </div>
               <h1 className="price__heading">Fried Chicken</h1>
               <p className="price__text">
@@ -73,7 +73,7 @@ const Prices = () => {
           <div className="col-4">
             <div className="price">
               <div className="price__img">
-                <img src="/img/wings.jpg" alt="wings" />
+                <img src="https://uspizza.in/images/home-box6.jpg" alt="wings" />
               </div>
               <h1 className="price__heading">Wings World</h1>
               <p className="price__text">
